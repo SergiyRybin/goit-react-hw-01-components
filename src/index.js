d583a7212f2aa8ce225692936a3b6b98188d340c
SergiyRybin <serhiobax@gmail.com>
2022-04-29 14:23:31 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import user from './path/user.json';
+
 // import { App } from 'components/App';
 // import './index.css';
 
@@ -8,5 +11,44 @@ import React from 'react';
 //     <App />
 //   </React.StrictMode>
 // );
-const e = React.createElement('div');
-console.log("e")
+
+// ReactDOM.render(cardUsr, document.querySelector('#root'));
+
+function Profile({ username, tag, location, avatar, stats }) {
+ 
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt={username} class="avatar" width="200" />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity"> {stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity"> {stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity"> {stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+createRoot(document.querySelector('#root')).render(
+  <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+);
