4217f2cdbcb2c9a4f58fb401d17daf1cd797924d
SergiyRybin <serhiobax@gmail.com>
2022-05-01 18:54:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
-
 import './index.css';
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
